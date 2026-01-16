5f0d925b9546e8b41ea27ad47f03eb1561214cf3
Gena <genada89@gmail.com>
2026-01-16 15:57:25 +0200

    ssl sert, https

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,6 +72,23 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 # ---------------------------------------------------
+# SECURITY (HTTPS)
+# ---------------------------------------------------
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    # HSTS
+    SECURE_HSTS_SECONDS = 31536000  # 1 рік
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    # Додаткові заголовки безпеки
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = "DENY"
+
+# ---------------------------------------------------
 # URLs / WSGI / ASGI
 # ---------------------------------------------------
 ROOT_URLCONF = "backend.urls"
